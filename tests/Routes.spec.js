1c68e7cd8d5493e596c42daaa948d088f0f5f2d7
zacscodingclub <zbaston@gmail.com>
2016-07-31 17:50:13 -0500

    Done.

--- tests/Routes.spec.js
+++ tests/Routes.spec.js
@@ -15,5 +15,45 @@ describe('Routes', function () {
 
 			expect(state.url).toEqual('/settings');
 		});
+
+		it('should have the correct template', function() {
+			state = $state.get('settings');
+
+			expect(state.templateUrl).toEqual('views/settings.html');
+		});
 	});
+
+	describe('settings.user', function () {
+		var state;
+
+		it('should have the correct URL', function () {
+			state = $state.get('settings.user');
+
+			expect(state.url).toEqual('/user');
+		});
+
+		it('should have the correct template', function() {
+			state = $state.get('settings.user');
+
+			expect(state.templateUrl).toEqual('views/settings/user.html');
+		});
+	});
+
+	describe('Settings.notifications', function () {
+		var state;
+
+		it('should have the correct URL', function () {
+			state = $state.get('settings.notifications');
+
+			expect(state.url).toEqual('/notifications');
+		});
+
+		it('should have the correct template', function() {
+			state = $state.get('settings.notifications');
+
+			expect(state.templateUrl).toEqual('views/settings/notifications.html');
+		});
+	});
+
+
 });
